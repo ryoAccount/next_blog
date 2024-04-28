0a295b52e7a14f2fe46eaee3073e057849a444e3
ryoAccount <jane.doe3wtp4649@gmail.com>
2024-04-29 04:17:51 +0900

    GA4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from '@vercel/analytics/react';
 import HeadData from './head'
@@ -11,6 +12,8 @@ export default function App({ Component, pageProps }: AppProps) {
       <Component {...pageProps} />
       <Analytics />
       <SpeedInsights />
+      <GoogleTagManager gtmId="GTM-KBNFW5RX" />
+      <GoogleAnalytics gaId="G-EFS8T077YL" />
     </>
   )
 }
