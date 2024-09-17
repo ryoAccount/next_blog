749ba744c9dac0afe815ccedaed1c02c5f205b95
ryoAccount <jane.doe3wtp4649@gmail.com>
2024-09-18 01:38:13 +0900

    meta httpEquiv

--- pages/head.tsx
+++ pages/head.tsx
@@ -3,12 +3,12 @@ import Head from 'next/head'
 export default function HeadData() {
   return (
     <Head>
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
       <meta name="msapplication-tap-highlight" content="no" />
       <meta name="description" content="Hello, I'm RYO. Welcome to RYO CV Monologue." />
       <meta name="keywords" content="RYO, CV, Monologue, RYO_nami, ryoAccount" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="author" content="RYO" />
       <meta name="theme-color" content="#629fd5" />
       <title>Ryo CV | Monologue</title>
